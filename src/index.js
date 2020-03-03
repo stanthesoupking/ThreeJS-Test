92f1e80d5e607fc659aa4ff7a853efec1a097ca7
Stanley Fuller <stanthesoupking@gmail.com>
2020-03-03 22:06:36 +1100

    Update

--- src/index.js
+++ src/index.js
@@ -5,7 +5,9 @@
 import * as THREE from 'three';
 
 var camera, scene, renderer;
-var geometry, material, mesh, light;
+var buildingGeometry, buildingMaterial, buildingMesh;
+var groundGeometry, groundMaterial, groundMesh;
+var light;
 
 const SCREEN_DIM = {
     width: 800,
@@ -17,22 +19,38 @@ animate();
 
 function init() {
 
-    camera = new THREE.PerspectiveCamera(70, SCREEN_DIM.width / SCREEN_DIM.height, 0.01, 10);
-    camera.position.z = 1;
+    camera = new THREE.PerspectiveCamera(70, SCREEN_DIM.width / SCREEN_DIM.height, 0.01, 100);
+    camera.position.z = 3.0;
+    camera.position.y = 1.3;
 
     scene = new THREE.Scene();
 
-    geometry = new THREE.BoxGeometry(0.2, 0.2, 0.2);
-    material = new THREE.MeshStandardMaterial();
+    buildingGeometry = new THREE.BoxGeometry(0.5, 0.5, 0.5);
+    buildingMaterial = new THREE.MeshStandardMaterial();
+    buildingMesh = new THREE.Mesh(buildingGeometry, buildingMaterial);
+    buildingMesh.position.y = 0.25;
+    buildingMesh.castShadow = true;
+    scene.add(buildingMesh);
 
-    mesh = new THREE.Mesh(geometry, material);
-    scene.add(mesh);
+    groundGeometry = new THREE.PlaneGeometry(10.0, 10.0);
+    groundMaterial = new THREE.MeshStandardMaterial();
+    groundMaterial.color.setRGB(0, 1.0, 0);
+    groundMesh = new THREE.Mesh(groundGeometry, groundMaterial);
+    groundMesh.setRotationFromAxisAngle(new THREE.Vector3(1, 0, 0), - Math.PI / 2.0);
+    groundMesh.receiveShadow = true;
+    scene.add(groundMesh);
 
-    light = new THREE.PointLight();
-    light.position.set(-1.0, 2.0, 2.0);
+    light = new THREE.DirectionalLight(0xffffff, 0.5);
+    light.position.set(0.5, 0.8, 0.3);
+    light.castShadow = true;
     scene.add(light);
 
+    var ambientLight = new THREE.AmbientLight(0xffffff, 0.15);
+    scene.add(ambientLight);
+
     renderer = new THREE.WebGLRenderer({ antialias: true });
+    renderer.shadowMap.enabled = true;
+    renderer.setClearColor(new THREE.Color(0x80bfff));
     renderer.setSize(SCREEN_DIM.width, SCREEN_DIM.height);
     document.getElementsByClassName('application-context')[0].appendChild(renderer.domElement);
 }
@@ -40,9 +58,6 @@ function init() {
 function animate() {
 
     requestAnimationFrame(animate);
-
-    mesh.rotation.x += 0.01;
-    mesh.rotation.y += 0.01;
 
     renderer.render(scene, camera);
 
