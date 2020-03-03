a6b471c60cf415cd4eaa62a991aa95ff02c8698a
Stanley Fuller <stanthesoupking@gmail.com>
2020-03-03 22:26:19 +1100

    Clickable building test

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@
 
 import * as THREE from 'three';
 
-var camera, scene, renderer;
+var camera, scene, renderer, raycaster;
 var buildingGeometry, buildingMaterial, buildingMesh;
 var groundGeometry, groundMaterial, groundMesh;
 var light;
@@ -20,8 +20,12 @@ animate();
 function init() {
 
     camera = new THREE.PerspectiveCamera(70, SCREEN_DIM.width / SCREEN_DIM.height, 0.01, 100);
-    camera.position.z = 3.0;
-    camera.position.y = 1.3;
+    camera.position.z = 2.0;
+    camera.position.y = 3.0;
+    camera.rotateX(-1.0);
+    camera.updateMatrixWorld();
+
+    raycaster = new THREE.Raycaster();
 
     scene = new THREE.Scene();
 
@@ -30,6 +34,7 @@ function init() {
     buildingMesh = new THREE.Mesh(buildingGeometry, buildingMaterial);
     buildingMesh.position.y = 0.25;
     buildingMesh.castShadow = true;
+    buildingMesh.name = 'Building';
     scene.add(buildingMesh);
 
     groundGeometry = new THREE.PlaneGeometry(10.0, 10.0);
@@ -53,6 +58,15 @@ function init() {
     renderer.setClearColor(new THREE.Color(0x80bfff));
     renderer.setSize(SCREEN_DIM.width, SCREEN_DIM.height);
     document.getElementsByClassName('application-context')[0].appendChild(renderer.domElement);
+
+    renderer.domElement.addEventListener("click", (event) => {
+        doClick(
+            new THREE.Vector2(
+                (event.offsetX / SCREEN_DIM.width) * 2 - 1,
+                - (event.offsetY / SCREEN_DIM.height) * 2 + 1
+            )
+        );
+    });
 }
 
 function animate() {
@@ -61,4 +75,22 @@ function animate() {
 
     renderer.render(scene, camera);
 
+}
+
+function doClick(mousePos) {
+    raycaster.setFromCamera(mousePos, camera);
+    var intersects = raycaster.intersectObjects( scene.children );
+    
+    let selectBuilding = false;
+    for (let x of intersects) {
+        if (x.object.name === 'Building') {
+            selectBuilding = true;
+        }
+    }
+
+    if (selectBuilding) {
+        buildingMaterial.color = new THREE.Color(0xff0000);
+    } else {
+        buildingMaterial.color = new THREE.Color(0xffffff)
+    }
 }
